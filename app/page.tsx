c348f33a6a48f8b1990f6e4a35685ac075a50569
Vivek5419 <vivek.5419kumar@gmail.com>
2025-08-25 21:37:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -316,18 +316,16 @@ export default function Home() {
               <div className="space-y-6">
                 <div className="text-center">
             <div className="blur-text-container" data-delay="100" data-animate-by="words" data-direction="top">
- <BlurText
+ 
+<h2
+  <BlurText
   text="Create with Visura"
   delay={150}
   animateBy="words"
   direction="top"
   onAnimationComplete={() => console.log("Animation completed!")}
-  className="text-2xl font-semibold mb-2 bg-gradient-to-r from-gray-400 to-white bg-clip-text text-transparent"
-/>
-<h2 className="text-2xl font-semibold mb-2 bg-gradient-to-r from-gray-400 to-white bg-clip-text text-transparent">
- Create with Visura
-  </h2>
-              
+  className="text-4xl font-semibold mb-2 bg-gradient-to-r from-gray-400 to-white bg-clip-text text-transparent"
+              />
                 </div>
                   <p className="text-muted-foreground">Transform your imagination into stunning visuals</p>
                 </div>
